6a7bfc4634b5b980eecf8767367f1aa3ed7ec0cb
Elías <eliasmora94@gmail.com>
2022-07-19 10:43:11 -0400

    Estilos en grilla de productos

--- src/app/productos.service.ts
+++ src/app/productos.service.ts
@@ -9,7 +9,7 @@ import { Product } from './products';
 export class ProductosService {
 
   //variables
-  URL_API = 'http://172.16.2.205:8080/productos';
+  URL_API = 'http://172.16.3.75:8080/productos';
 
   constructor(private http: HttpClient) { }
 
